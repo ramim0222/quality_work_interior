a740519667035460ca75b9f17d44ff07d9d450ab
S. M. Ramim <162826402+ramim0222@users.noreply.github.com>
2026-06-29 02:25:29 +0600

    First commit after setting up the env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            User::factory()->make([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-            ])->getAttributes(),
+            ['email' => 'qualityworkinterior@gmail.com'],
+            [
+                'name' => 'MD. Rasel Seikh',
+                'password' => 'password',
+            ],
         );
     }
 }
